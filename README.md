cd806bba39cad6b455125578d8fe4bce9df6b87e
leslie <waretaradoli@gmail.com>
2016-12-02 18:06:43 +0300

    add files

---
+++ README.md
@@ -0,0 +1,12 @@
+1. Get user  input
+Example:15
+2. have an empty array
+Example: Array[]
+3. have the range of the numbers from one to the input and put them in the array.
+Example: Array[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+4. replace numbers divisible to three "ping"
+Example: Array[1 , 2,ping,4, 5 ,ping ,7,8 ,ping ,10,11,ping ,13 , 14 ,ping]
+5. replace numbers divisible to five  replace with "pong"
+Example: Array[1 , 2,ping,4, pong ,ping ,7,8 ,ping ,pong,11,ping ,13 , 14 ,ping]
+6. replace numbers divisible to both three and five and replace  with "pingpong"
+Example: Array[1 , 2,ping,4, pong ,ping ,7,8 ,ping ,pong,11,ping ,13 , 14 ,pingpong]
---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+   $("#sub").click(function(event) {
+    event.preventDefault();
+    var Number = $("#number").val();
+    console.log(Number);
+     var num=[];
+  for (var i=1;i<=Number;i++){
+      if(i % 15===0){
+         num.push("pingpong");
+       }
+       else if(i%5===0){
+         num.push("ping");
+       }
+       else if (i% 3=== 0){
+         num.push("pong");
+       }
+       else{
+         num.push(i);
+       }
+     for (i = 0; i<=number.length; i++){
+         $("#display").html("<li>"+num[i]+"</li>");
+     }
+     $("#display").append("<li>"+num[i]+"</li>");
+      console.log(num);
+   });
+
+ });
